71588b8766cbae7e2ae039215fe9875963d08750
Polaris-algedi <102882133+Polaris-algedi@users.noreply.github.com>
2023-06-11 02:19:38 +0100

    Fix max_integer, the function now handles negative integers correctly

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -11,8 +11,8 @@ def max_integer(my_list=[]):
     """
     if len(my_list) == 0:
         return None
-    max_i = 0
-    for i in my_list:
+    max_i = my_list[0]
+    for i in my_list[1:]:
         if i > max_i:
             max_i = i
     return max_i
