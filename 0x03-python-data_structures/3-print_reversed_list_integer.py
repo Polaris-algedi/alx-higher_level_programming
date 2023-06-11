b5d4421b7cea95abc0084efc89d6d3f68aa9a9e0
Polaris-algedi <102882133+Polaris-algedi@users.noreply.github.com>
2023-06-11 03:40:03 +0100

    Check if the list is not empty and add documentation

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for number in reversed(my_list):
-        print("{:d}".format(number))
+    """Function that prints all integers of a list,
+    in reverse order.
+
+    Args:
+        my_list: list of integers
+    """
+    if my_list:
+        for number in my_list[::-1]:
+            print("{:d}".format(number))
