1f17c346635d59981347883573b4e23b4f42b698
Plaris-algedi <karchalmohammed@gmail.com>
2023-06-14 09:27:01 -0700

    Add a function that computes the square value of all integers of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """Computes the square value of all integers of a matrix
+
+    Args:
+        matrix: 2d list of integers
+    """
+    return list(map(lambda row : [x**2 for x in row], matrix))
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+Learn Python - More Data Structures: Set, Dictionary
