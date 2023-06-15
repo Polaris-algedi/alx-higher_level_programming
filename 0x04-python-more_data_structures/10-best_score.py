a0c383897d2b77ad1afdd1ea38f99fa549dc8a17
Plaris-algedi <karchalmohammed@gmail.com>
2023-06-14 17:05:33 -0700

    Return the argument checker

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,4 +8,5 @@ def best_score(a_dictionary):
     Returns:
         a key with the biggest integer value
     """
-    return max(a_dictionary, key=a_dictionary.get)
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
