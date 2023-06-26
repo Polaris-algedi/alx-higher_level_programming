964fed4d57a81081320f815e429c67976eaadef6
Plaris-algedi <karchalmohammed@gmail.com>
2023-06-26 11:26:56 -0700

    Add the Clean-up Actions clause to the function

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,18 +13,18 @@ def list_division(my_list_1, my_list_2, list_length):
     Returns:
         The new list (length = list_length) with all divisions.
     """
-    result = []
+    new_list = []
     for i in range(list_length):
+        result = 0
         try:
-            result.append(my_list_1[i] / my_list_2[i])
+            result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
-            result.append(0)
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
         except IndexError:
             print("out of range")
-            result.append(0)
+        finally:
+            new_list.append(result)
 
-    return result
+    return new_list
