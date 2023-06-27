be30c6f065c81de8a5a32e5f1c82f9f2bc32b9ed
Plaris-algedi <karchalmohammed@gmail.com>
2023-06-26 18:17:53 -0700

    Add a function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    """Executes a function safely
+
+    Args:
+        fct: pointer to a function
+        args: variadic argument
+
+    Returns:
+        The result of the function, otherwise returns
+        None if something happens during the function
+        and prints in stderr the error precede by Exception:
+    """
+    try:
+        return fct(*args)
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return None
