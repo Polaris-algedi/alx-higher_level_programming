ddcec9ddb920aaa60257674bb713d13dc2f62460
Plaris-algedi <karchalmohammed@gmail.com>
2023-06-27 11:24:09 -0700

    Add to the square class an optional instantiation and a type/value verification

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes instance attributes
+
+        Args:
+            size: size of the square
+
+        Raises:
+            TypeError: If 'size' is not an integer
+            ValueError: If 'size' is less than 0.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
