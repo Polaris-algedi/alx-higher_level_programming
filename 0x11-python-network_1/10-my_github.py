e6a49480dec7bb0189a0cb7b3e105ecf85963ee5
Plaris-algedi <karchalmohammed@gmail.com>
2023-09-29 19:34:28 -0700

    Fix style

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-This module sends a request to the GitHub API and displays the user's id.
-"""
+"""Sends a request to the GitHub API and displays the user's id"""
 
-import requests
-import sys
+if __name__ == '__main__':
+    import requests
+    import sys
 
-username = sys.argv[1]
-token = sys.argv[2]
+    username = sys.argv[1]
+    token = sys.argv[2]
 
-response = requests.get('https://api.github.com/user', auth=(username, token))
-print(f"{response.json().get('id')}")
+    response = requests.get('https://api.github.com/user',
+                            auth=(username, token))
+    print(f"{response.json().get('id')}")
