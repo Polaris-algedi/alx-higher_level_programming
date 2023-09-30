7541aaf6472ac14449ac4b9b1d4e89e321d3db86
Plaris-algedi <karchalmohammed@gmail.com>
2023-09-29 19:12:38 -0700

    Importing modules should not be executed when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,10 +5,9 @@ from the response header.
 """
 
 
-import urllib.request
-import sys
-
 if __name__ == "__main__":
+    import urllib.request
+    import sys
     with urllib.request.urlopen(sys.argv[1]) as response:
         x_request_id = response.info()
         print(x_request_id.get('X-Request-Id'))
