e483fedd53d65d21eb19994f9278e4e0a5e56200
Plaris-algedi <karchalmohammed@gmail.com>
2023-09-29 19:04:35 -0700

    Use the method info to get the header dict

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,10 +4,11 @@ This module sends a request to a URL and displays the 'X-Request-Id'
 from the response header.
 """
 
+
 import urllib.request
 import sys
 
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
-        x_request_id = response.headers.get('X-Request-Id')
-        print(x_request_id)
+        x_request_id = response.info()
+        print(x_request_id.get('X-Request-Id'))
