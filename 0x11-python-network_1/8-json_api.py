ef1d7d1882a9c9816cc1906c4a2abfbeb79ea625
Plaris-algedi <karchalmohammed@gmail.com>
2023-09-29 19:31:23 -0700

    Fix style

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""
-This module sends a POST request to a URL with a letter as a parameter,
-and displays the body of the response.
-"""
+"""This module sends a POST request to a URL with a letter as a parameter,
+and displays the body of the response."""
 
-import requests
-import sys
+if __name__ == '__main__':
+    import requests
+    import sys
 
-if len(sys.argv) < 2:
-    q = ""
-else:
-    q = sys.argv[1]
-
-response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-
-try:
-    json_response = response.json()
-    if json_response:
-        print(f"[{json_response.get('id')}] {json_response.get('name')}")
+    if len(sys.argv) < 2:
+        q = ""
     else:
-        print('No result')
-except ValueError:
-    print('Not a valid JSON')
+        q = sys.argv[1]
+
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
